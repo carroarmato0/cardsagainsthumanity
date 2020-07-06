94c47ffab60dcec577b1ceb560148c304f680b22
Christope Vanlancker <carroarmato0@gmail.com>
2020-07-06 23:27:42 +0200

    Import and export Decks via Serialization through DeckManager
    Signed-off-by: Christope Vanlancker <carroarmato0@gmail.com>

---
+++ Unittests/test_deckmanager.py
@@ -0,0 +1,32 @@
+import os
+from unittest import TestCase
+
+from card import Card
+from deck import Deck
+from deckmanager import DeckManager
+
+
+class TestDeckManager(TestCase):
+
+    def setUp(self):
+        black_card = Card(type="black", content="Why did the chicken cross the road?", answer_count=1)
+        white_card1 = Card(type="white", content="A homoerotic subplot")
+        white_card2 = Card(type="white", content="To get to the other side")
+        card_list = [black_card, white_card1, white_card2]
+        self.deck = Deck(id=0, name="Animals", description="The animal pack", cards=card_list)
+        self.manager = DeckManager()
+
+    def test_save(self):
+        self.manager.save(self.deck)
+        self.assertTrue(os.path.exists(str(self.deck.id) + ".json"))
+
+    def test_load(self):
+        self.manager.save(self.deck)
+        deck = self.manager.load(uri=str(self.deck.id) + ".json")
+        self.assertTrue(isinstance(deck, Deck))
+
+    def tearDown(self):
+        try:
+            os.remove(str(self.deck.id) + ".json")
+        except FileNotFoundError:
+            pass
---
+++ deckmanager.py
@@ -0,0 +1,42 @@
+import urllib.request
+import json
+
+from deck import Deck
+from utils import convert_to_dict, dict_to_obj
+
+
+class DeckManager:
+    """ This library helps loading and writing card Decks """
+
+    def load(self, uri=None):
+        """ Load a json encoded deck from a URL or path """
+        deck = None
+
+        if uri is None:
+            raise ValueError(f"Empty uri has been given to load a deck")
+        else:
+            """ Attempt to load the json data as a url """
+            try:
+                with urllib.request.urlopen(uri) as url:
+                    data = json.loads(url.read().decode())
+                    deck = json.loads(data, object_hook=dict_to_obj)
+            except ValueError:
+                try:
+                    """ Attempt to load the json data as a file """
+                    with open(uri, 'r') as reader:
+                        deck = json.loads(reader.read(), object_hook=dict_to_obj)
+                except FileNotFoundError:
+                    pass
+        return deck
+
+    def save(self, deck=None):
+        """ Save a Deck of cards to the filesystem """
+        if not isinstance(deck, Deck):
+            raise ValueError(f"The supplied deck is not recognized")
+        else:
+            """
+            deck_json = DeckEncoder().encode(deck)
+            """
+            deck_json = json.dumps(deck, default=convert_to_dict, indent=4, sort_keys=True)
+            with open(str(deck.id) + '.json', 'w') as writer:
+                writer.write(deck_json)
