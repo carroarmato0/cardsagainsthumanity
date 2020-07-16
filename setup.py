064938984fadc604cb2b61720de768d58ee25e46
Christope Vanlancker <carroarmato0@gmail.com>
2020-07-16 20:06:55 +0200

    Add basic setup.py
    Signed-off-by: Christope Vanlancker <carroarmato0@gmail.com>

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='Cah',
+      version='0.1',
+      description='A Python implementation of Cards Against Humanity',
+      author='Christophe Vanlancker',
+      author_email='carroarmato0@gmail.com',
+      url='https://github.com/carroarmato0/cardsagainsthumanity',
+      packages=find_packages(),
+      )
