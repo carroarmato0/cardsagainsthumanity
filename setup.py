c3f3bc16b6a85adcf889c48f55fbfd784b06012b
Christope Vanlancker <carroarmato0@gmail.com>
2020-07-18 00:21:16 +0200

    Add setup.py and necessary other files
    Signed-off-by: Christope Vanlancker <carroarmato0@gmail.com>

--- setup.py
+++ setup.py
@@ -2,11 +2,22 @@
 
 from setuptools import setup, find_packages
 
+with open("./README.md") as f:
+    long_description = f.read()
+
 setup(name='Cah',
       version='0.1',
       description='A Python implementation of Cards Against Humanity',
+      long_description=long_description,
       author='Christophe Vanlancker',
       author_email='carroarmato0@gmail.com',
       url='https://github.com/carroarmato0/cardsagainsthumanity',
       packages=find_packages(),
+      include_package_data=True,
+      install_requires=[
+          'bottle',
+          'bottle-mongo',
+          'pymongo',
+          'iso-639',
+      ],
       )
